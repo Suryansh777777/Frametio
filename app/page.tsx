8cb323fa6861e1dc3b3767a6fda974ede47b2b2a
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-25 16:10:15 +0530

    update page layout and remove unnecessary CSS class

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen md:items-center md:justify-center bg-black/[0.96] antialised bg-grid/white relative overflow-hidden">
+    <div className="w-full  md:items-center md:justify-center bg-black/[0.96] antialised bg-grid/white relative overflow-hidden">
       <Navbar />
       <Spotlight
         className="hidden md:flex md:left-90 md:-top-80"
